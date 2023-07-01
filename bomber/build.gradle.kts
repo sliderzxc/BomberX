3af58db907c8ee4d0b9f174106acdc96e4a29ec6
vadwwxz <vadwwxz@gmail.com>
2023-07-01 13:55:35 +0300

    Initialized new module - bomber

---
+++ bomber/build.gradle.kts
@@ -0,0 +1,42 @@
+plugins {
+    id(Plugins.Library.dependency)
+    id(Plugins.Android.dependency)
+}
+
+android {
+    namespace = Config.coreModulePackageName
+    compileSdk = Config.compileSdk
+
+    defaultConfig {
+        minSdk = Config.minSdk
+        targetSdk = Config.targetSdk
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        vectorDrawables.useSupportLibrary = true
+    }
+
+    buildTypes {
+        @Suppress("UnstableApiUsage")
+        release {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_18
+        targetCompatibility = JavaVersion.VERSION_18
+    }
+    kotlinOptions {
+        jvmTarget = Config.jvmTarget
+    }
+    viewBinding.enable = true
+}
+
+dependencies {
+    // Basic
+    implementation(Dependencies.Android.coreKtx)
+    implementation(Dependencies.Android.appCompat)
+    implementation(Dependencies.Android.constraintLayout)
+    implementation(Dependencies.Android.material)
+    implementation(Dependencies.Android.fragment)
+}
