1f541ea3ed511febd1e8b268fda63d73ae5c1b97
vadwwxz <vadwwxz@gmail.com>
2023-07-01 22:59:07 +0300

    Created dark theme, new colors

--- app/src/main/java/com/main/bomberx/presentation/ui/MainActivity.kt
+++ app/src/main/java/com/main/bomberx/presentation/ui/MainActivity.kt
@@ -2,13 +2,20 @@ package com.main.bomberx.presentation.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelStoreOwner
 import com.main.bomberx.databinding.ActivityMainBinding
+import com.main.core.sl.ProvideViewModel
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), ProvideViewModel {
     private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+    }
+
+    override fun <T : ViewModel> provideViewModel(clazz: Class<T>, owner: ViewModelStoreOwner): T {
+        return (application as ProvideViewModel).provideViewModel(clazz, owner)
     }
 }
--- bomber/src/main/java/com/main/bomber/presentation/ui/BomberFragment.kt
+++ bomber/src/main/java/com/main/bomber/presentation/ui/BomberFragment.kt
@@ -1,11 +1,8 @@
 package com.main.bomber.presentation.ui
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import android.widget.AdapterView
-import androidx.fragment.app.Fragment
 import com.main.bomber.presentation.viewmodel.BomberViewModel
 import com.main.core.R
 import com.main.core.data.BaseFragment
@@ -22,7 +19,8 @@ class BomberFragment : BaseFragment<BomberViewModel>() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.phoneCode.adapter = CountryCodeAdapter(requireActivity(), COUNTRY_FLAGS, COUNTRY_CODES)
+        val counterCodeAdapter = CountryCodeAdapter(requireActivity(), COUNTRY_FLAGS, COUNTRY_CODES)
+        binding.phoneCode.adapter = counterCodeAdapter
 
         val hints = resources.getStringArray(R.array.hints)
         binding.phoneNumber.hint = hints[0]
@@ -30,13 +28,15 @@ class BomberFragment : BaseFragment<BomberViewModel>() {
 
         binding.phoneCode.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
             override fun onItemSelected(
-                adapterView: AdapterView<*>?, view: View,
-                index: Int, l: Long,
+                adapterView: AdapterView<*>?,
+                selectedItemView: View?, index: Int, l: Long,
             ) {
-                binding.phoneNumber.hint = hints[index]
+                selectedItemView?.let { _ ->
+                    binding.phoneNumber.hint = hints[index]
+                }
             }
 
-            override fun onNothingSelected(adapterView: AdapterView<*>?) {}
+            override fun onNothingSelected(adapterView: AdapterView<*>?) = Unit
         }
     }
 }
